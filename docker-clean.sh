11dceeab76055bbd9d5a3327d7437c58f060b887
Mark Waite <mark.earl.waite@gmail.com>
2018-09-12 16:43:04 -0600

    Use --no-run-if-empty in xargs on Linux clean
    Not available on macOS, but works well on Linux.

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-docker ps -a -q | xargs docker rm -v
-docker images -f dangling=true -q | xargs docker rmi
-docker volume ls -q | xargs docker volume rm
-docker network ls | grep default | awk '{print $1}' | xargs docker network rm
+xargs_option=
+if [ "$(uname)" == "Linux" ]
+then
+    xargs_option="--no-run-if-empty"
+fi
+
+docker ps -a -q | xargs $xargs_option docker rm -v
+docker images -f dangling=true -q | xargs $xargs_option docker rmi
+docker volume ls -q | xargs $xargs_option docker volume rm
+docker network ls | grep default | awk '{print $1}' | xargs $xargs_option docker network rm
