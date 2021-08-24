093f84ea20bc590a78029b9673d757e4eb331856
David Schott <schottdb@gmail.com>
2021-08-24 19:18:36 -0400

    new way to clean

--- docker-clean.sh
+++ docker-clean.sh
@@ -1,12 +1,2 @@
 #!/bin/sh
-
-xargs_option=
-if [ "$(uname)" == "Linux" ]
-then
-    xargs_option="--no-run-if-empty"
-fi
-
-docker ps -a -q | xargs $xargs_option docker rm -v
-docker images -f dangling=true -q | xargs $xargs_option docker rmi
-docker volume ls -q | xargs $xargs_option docker volume rm
-docker network ls | grep default | awk '{print $1}' | xargs $xargs_option docker network rm
+docker system prune
